d0579f1aa176a32227ee08ca8e6b0ae83b58e370
dumaike <mdwyer84@gmail.com>
2014-12-16 22:00:07 -0500

    Added the ability to cast from multiple spots per character

---
+++ DungeonGuide/Assets/Scripts/GameObjectUtility.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectUtility
+{
+	#region public methods
+	public static void SetLayerRecursive(int layerIndex, Transform root)
+	{
+		root.gameObject.layer = layerIndex;
+		for (int iChildIndex = 0; iChildIndex < root.transform.childCount; ++iChildIndex)
+		{
+			SetLayerRecursive(layerIndex, root.transform.GetChild(iChildIndex));
+		}
+	}
+	#endregion
+
+	#region private methods
+
+	#endregion
+}
+
--- DungeonGuide/Assets/Scripts/PlayerCharacterRoot.cs
+++ DungeonGuide/Assets/Scripts/PlayerCharacterRoot.cs
@@ -1,25 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace DungeonGuide
 {
 	public class PlayerCharacterRoot : CharacterRoot
     {
         private TileRoot[] allTiles;
+		private List<TileRoot> unseenTiles;
 
         private MeshRenderer[] meshRenderers;
         
         private const float VISION_DISTANCE = 20.0f;
-        private Vector3 eyeLevel = new Vector3(0, 0.1f, 0);
+
+		private const float HALF_TILE_WIDTH = 0.3f;
+		private Vector3[] visionPoints = {
+			new Vector3(HALF_TILE_WIDTH, 0.1f, HALF_TILE_WIDTH), 
+			new Vector3(-HALF_TILE_WIDTH, 0.1f, HALF_TILE_WIDTH), 
+			new Vector3(HALF_TILE_WIDTH, 0.1f, -HALF_TILE_WIDTH), 
+			new Vector3(-HALF_TILE_WIDTH, 0.1f, -HALF_TILE_WIDTH) 
+		};
         
         private Color selectedColor = new Color(0, 1, 1, 1);
         private Color defaultColor = new Color(1, 1, 1, 1);
+
+		public const string INVISIBLE_LAYER_NAME = "NonSightBlocking";
 
 		#region initializers
 		private void Awake()
 		{
             this.allTiles = GameObject.FindObjectsOfType<TileRoot>();
             this.meshRenderers = this.GetComponentsInChildren<MeshRenderer> ();
+
+			GameObjectUtility.SetLayerRecursive (LayerMask.NameToLayer (INVISIBLE_LAYER_NAME), this.transform); 
 		}
 
 		private void Start()
@@ -39,19 +52,41 @@ namespace DungeonGuide
 
 		#region private methods
         private void Update()
-        {
-            Vector3 characterEyeLevel = this.transform.position + eyeLevel;
-            foreach (TileRoot curTile in this.allTiles)
-            {
-                Ray raycastRay = new Ray(characterEyeLevel, curTile.transform.position - characterEyeLevel);
-                RaycastHit hitInfo = new RaycastHit();
-                Physics.Raycast(raycastRay, out hitInfo, VISION_DISTANCE);
-                float distanceFromHitToTile = Vector3.Distance(hitInfo.point, curTile.transform.position);
-                bool showTile = distanceFromHitToTile < 0.45;
-                curTile.ShowTile(showTile);                   
+		{
+			int layerMask = 1 << 0;
 
-                Debug.DrawLine(raycastRay.origin, hitInfo.point);
-            }
+            this.unseenTiles = new List<TileRoot>(this.allTiles);
+			foreach (Vector3 localVisionPoint in this.visionPoints) 
+			{
+				List<TileRoot> unseenLeftovers = new List<TileRoot> ();
+				Vector3 visionPoint = this.transform.position + localVisionPoint;
+
+				foreach (TileRoot curTile in this.unseenTiles) 
+				{
+					Ray raycastRay = new Ray (visionPoint, curTile.transform.position - visionPoint);
+					RaycastHit hitInfo = new RaycastHit ();
+					Physics.Raycast (raycastRay, out hitInfo, VISION_DISTANCE, layerMask);
+					float distanceFromHitToTile = Vector3.Distance (hitInfo.point, curTile.transform.position);
+					bool showTile = distanceFromHitToTile < 0.45;
+					if (showTile)
+					{
+						curTile.ShowTile (showTile);        
+					}
+					else
+					{
+						unseenLeftovers.Add(curTile);
+					}
+
+					Debug.DrawLine (raycastRay.origin, hitInfo.point);
+				}
+
+				this.unseenTiles = new List<TileRoot>(unseenLeftovers);
+			}
+
+			foreach (TileRoot unseenTile in this.unseenTiles) 
+			{
+				unseenTile.ShowTile(false);
+			}
         }
 
         public void CharacterSelected(bool selected)
