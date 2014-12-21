a2d9920c424f5a4cc0cfc10e22b3383bcd247887
dumaike <mdwyer84@gmail.com>
2014-12-21 18:28:22 -0500

    First pass setting up a dock in GUI.

---
+++ DungeonGuide/Assets/GuiAnimationsController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DungeonGuide
+{
+	public class GuiAnimationsController : MonoBehaviour
+	{
+		#region initializers
+		private void Awake()
+		{
+
+		}
+
+		private void Start()
+		{
+
+		}
+
+		private void OnDestroy()
+		{
+
+		}
+		#endregion
+
+		#region public methods
+		public void ToggleDock()
+		{
+
+		}
+		#endregion
+
+		#region private methods
+
+		#endregion
+	}
+
+}
+
