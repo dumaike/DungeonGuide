a0c7e05250c8e11f9b42e7a216615c162d1d843c
dumaike <mdwyer84@gmail.com>
2014-12-26 16:57:39 -0500

    Added a scaling dock to the left of the screen.

--- DungeonGuide/Assets/Editor/TileRootEditor.cs
+++ DungeonGuide/Assets/Editor/TileRootEditor.cs
@@ -10,7 +10,6 @@ namespace DungeonGuide
 	public class TileRootEditor : Editor
 	{
 		private TileRoot tile;
-		private Vector3 oldPosition;
 		#region public methods
 
 		#endregion
@@ -19,29 +18,40 @@ namespace DungeonGuide
 		public override void OnInspectorGUI ()
 		{
 			base.OnInspectorGUI ();
+
 			if (this.tile == null)
 			{
 				this.tile = target as TileRoot;
 			}
-
-			if (this.tile.transform.parent == null && this.tile.gameObject.activeInHierarchy)
+			
+			Log.Print("Running OnInspectorGUI for tile: " + this.tile.name);
+			
+			int snappedX = (int)this.tile.transform.position.x;
+			int snappedY = (int)this.tile.transform.position.y;
+			if (snappedX != this.tile.transform.position.x ||
+			    snappedY != this.tile.transform.position.y)
 			{
-				GameObject tileRoot = GameObject.Find("Tiles");
-				if (tileRoot == null)
-				{
-					Debug.LogError("Could not find an object named \"TileRoot\" to place the tile under. Please Create one!");
-				}
-				else
-				{
-					this.tile.transform.parent = tileRoot.transform;
-				}
-			}
+				Log.Print("Snapping Tile: " + this.tile.name);
 
-			Vector3 snappedPosition = this.tile.transform.position;
-			snappedPosition.x = (float)Math.Round (snappedPosition.x);
-			snappedPosition.z = (float)Math.Round (snappedPosition.z);
-			snappedPosition.y = 0;
-			this.tile.transform.position = snappedPosition;
+					if (this.tile.transform.parent == null && this.tile.gameObject.activeInHierarchy)
+					{
+						GameObject tileRoot = GameObject.Find("Tiles");
+						if (tileRoot == null)
+						{
+							Log.Error("Could not find an object named \"TileRoot\" to place the tile under. Please Create one!");
+						}
+						else
+						{
+							this.tile.transform.parent = tileRoot.transform;
+						}
+					}
+
+					Vector3 snappedPosition = this.tile.transform.position;
+					snappedPosition.x = (float)Math.Round (snappedPosition.x);
+					snappedPosition.z = (float)Math.Round (snappedPosition.z);
+					snappedPosition.y = 0;
+					this.tile.transform.position = snappedPosition;
+			}
 		}
 		#endregion
 	}
