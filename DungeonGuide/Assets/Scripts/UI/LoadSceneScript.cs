8aebacf6c87b65f0fdb62db6b6241e2264c4a001
dumaike <mdwyer84@gmail.com>
2015-01-11 13:17:14 -0500

    Created a way to switch between scenes.

---
+++ DungeonGuide/Assets/Scripts/UI/LoadSceneScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DungeonGuide
+{
+	public class LoadSceneScript : MonoBehaviour
+	{
+		[SerializeField]
+		private string sceneName;
+	
+		#region public methods
+		public void LoadScene()
+		{
+			Application.LoadLevel(this.sceneName);
+		}
+		#endregion
+	}
+
+}
+
---
+++ DungeonGuide/Assets/Scripts/UI/MenuUi.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DungeonGuide
+{
+	public class MenuUi : MonoBehaviour
+	{	
+		#region initializers
+		private void Awake()
+		{
+			if (Time.time < 0.1f)
+			{
+				Log.Warning("You left the MenuUi active. Deactivating at startup, but you should really do this in the editor");
+				this.gameObject.SetActive(false);
+			}
+		}
+
+		private void Start()
+		{
+
+		}
+
+		private void OnDestroy()
+		{
+
+		}
+		#endregion
+
+		#region public methods
+		public void CloseMenu()
+		{
+			this.gameObject.SetActive(false);
+		}
+		
+		public void OpenMenu()
+		{
+			this.gameObject.SetActive(true);
+		}
+		#endregion
+
+		#region private methods
+
+		#endregion
+	}
+
+}
+
