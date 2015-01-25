fe49c82db2f55a2090b31e83d51dca1d2ca4345c
dumaike <mdwyer84@gmail.com>
2015-01-25 00:29:46 -0500

    Began redoing vision to be much more granular.

--- DungeonGuide/Assets/Scripts/Global/CharacterVisionController.cs
+++ DungeonGuide/Assets/Scripts/Global/CharacterVisionController.cs
@@ -5,23 +5,13 @@ using System.Collections.Generic;
 namespace DungeonGuide
 {
 	public class CharacterVisionController
-	{		
-		private TileRoot[] allTiles;
-		private List<TileRoot> unseenTiles;
-		
+	{				
 		private const float VISION_DISTANCE = 15.0f;
-		
-		public const float HALF_TILE_WIDTH = 0.40f;
-		private Vector3[] visionPoints = {
-			new Vector3(HALF_TILE_WIDTH, 0.1f, HALF_TILE_WIDTH), 
-			new Vector3(-HALF_TILE_WIDTH, 0.1f, HALF_TILE_WIDTH), 
-			new Vector3(HALF_TILE_WIDTH, 0.1f, -HALF_TILE_WIDTH), 
-			new Vector3(-HALF_TILE_WIDTH, 0.1f, -HALF_TILE_WIDTH) 
-		};      
-
-		public const bool DRAW_SIGHT_LINES = true;
+		private const int NUM_RAYS = 360;
 
 		public const string INVISIBLE_LAYER_NAME = "NonSightBlocking";
+		
+		private Vector3 VISION_OFFSET = new Vector3(0, 0.1f, 0);
 
 		private List<PlayerCharacterRoot> playerCharacters;
 		
@@ -30,8 +20,6 @@ namespace DungeonGuide
 		#region initializers
 		public CharacterVisionController()
 		{						
-			this.allTiles = GameObject.FindObjectsOfType<TileRoot>();
-			Log.Print("There are " + this.allTiles.Length + " tiles in the scene.", LogChannel.EDITOR_SETUP);
 			this.playerCharacters = new List<PlayerCharacterRoot>(GameObject.FindObjectsOfType<PlayerCharacterRoot>());
 		}
 		#endregion
@@ -67,80 +55,37 @@ namespace DungeonGuide
 		
 			int layerMask = 1 << 0;
 			
-			this.unseenTiles = new List<TileRoot> (this.allTiles);
-			
 			foreach (PlayerCharacterRoot player in this.playerCharacters) 
 			{
-				foreach (Vector3 localVisionPoint in this.visionPoints) 
+				Vector3 characterVisionOrigin = player.transform.position + VISION_OFFSET;
+				float raycastStep = 360/NUM_RAYS;
+				for (int iRayIndex = 0; iRayIndex < NUM_RAYS; ++iRayIndex)
 				{
-					List<TileRoot> unseenLeftovers = new List<TileRoot> ();
-					Vector3 visionPoint = player.transform.position + localVisionPoint;
+					Vector3 directionVector = Quaternion.Euler(0, iRayIndex*raycastStep, 0)*Vector3.right;
+					Ray raycastRay = new Ray(characterVisionOrigin, directionVector);
 					
-					foreach (TileRoot curTile in this.unseenTiles) 
-					{						
-						bool canSeeAllMeshesInTile = true;
-						foreach (MeshRenderer meshRenderer in curTile.allMeshRenderers)
-						{						
-							float distanceToMesh = (visionPoint - meshRenderer.transform.position).magnitude;
-							if (distanceToMesh > VISION_DISTANCE)
-							{
-								canSeeAllMeshesInTile = false;
-								break;
-							}
-							
-							//If the mesh doesn't even exist in the current game state
-							//don't bother raycasting
-							if (!curTile.DoesMeshExist(meshRenderer))
-							{
-								continue;
-							}
-						
-							//Cast a ray toward the tile
-							Ray raycastRay = new Ray (visionPoint, meshRenderer.transform.position - visionPoint);
-							RaycastHit hitInfo = new RaycastHit ();
-							Physics.Raycast (raycastRay, out hitInfo, VISION_DISTANCE, layerMask);
-						
-							//If it's close to the distance to the tile, we can see the tile
-							bool canSeeMesh = hitInfo.transform == meshRenderer.transform || 
-								distanceToMesh < hitInfo.distance || hitInfo.transform == null;
-							if (!canSeeMesh)
-							{
-								canSeeAllMeshesInTile = false;
-								break;
-							}
-#if DRAW_SIGHT_LINES
-							Debug.DrawLine (raycastRay.origin, hitInfo.point);
-#endif
-						}
-						
-						if (canSeeAllMeshesInTile)
-						{
-							curTile.ShowTile (canSeeAllMeshesInTile);        
-						} 
-						else 
-						{
-							unseenLeftovers.Add (curTile);
-						}
+					RaycastHit hitInfo = new RaycastHit ();
+					bool hitSomething = Physics.Raycast (raycastRay, out hitInfo, VISION_DISTANCE, layerMask);
+					Vector3 hitPoint;
+					if (hitSomething)
+					{
+						hitPoint = hitInfo.point;
+					}
+					else
+					{
+						hitPoint = characterVisionOrigin + directionVector*VISION_DISTANCE;
 					}
 					
-					this.unseenTiles = new List<TileRoot> (unseenLeftovers);
-				}
+					Debug.DrawRay(characterVisionOrigin, hitPoint - characterVisionOrigin);
+				}	
 			}
 			
-			foreach (TileRoot unseenTile in this.unseenTiles) 
-			{
-				unseenTile.ShowTile (false);
-			}
-			
-			this.visionDirty = false;
+			//this.visionDirty = false;
 		}
 		
 		public void ShowAllTiles()
 		{
-			foreach (TileRoot curTile in this.allTiles) 
-			{
-				curTile.ShowTile(true);
-			}
+		
 		}
 		#endregion
 
