a1b9c380cbb59ae2f975d73e53e982ff5af599db
dumaike <mdwyer84@gmail.com>
2015-02-25 21:43:42 -0500

    Filled out Sandrian's shop a bit. Added a duplicate destroy function.

--- DungeonGuide/Assets/Scripts/Editor/ImportPreferences.cs
+++ DungeonGuide/Assets/Scripts/Editor/ImportPreferences.cs
@@ -7,19 +7,21 @@ public class ImportPreferences : AssetPostprocessor
 	private void OnPostprocessTexture(Texture2D texture)
 	{
 		if (assetPath.Contains("CharacterSprites"))
-		{
+		{			
 			int lastSlashIndex = assetPath.LastIndexOf("/");
 			Log.Print("Importing texture \"" + assetPath.Substring(lastSlashIndex + 1) + "\" with custom character sprite settings", LogChannel.IMPORT);
 			TextureImporter texImporter = assetImporter as TextureImporter;
 			TextureImporterSettings textureSettings = new TextureImporterSettings();
 			textureSettings.ApplyTextureType(TextureImporterType.Sprite, true);
 			textureSettings.spritePixelsPerUnit = 100;
-			textureSettings.textureFormat = TextureImporterFormat.AutomaticCompressed;
+			textureSettings.textureFormat = TextureImporterFormat.RGBA32;
 			textureSettings.maxTextureSize = 512;
 			textureSettings.mipmapEnabled = false;
 			textureSettings.spriteMode = 1;
 			texImporter.spritePackingTag = "characters";
 			texImporter.SetTextureSettings(textureSettings);
+			
+			EditorUtility.CompressTexture(texture, TextureFormat.RGBA32, TextureCompressionQuality.Best);
 		}		
 	}
 }
--- DungeonGuide/Assets/Scripts/Editor/MenuItems.cs
+++ DungeonGuide/Assets/Scripts/Editor/MenuItems.cs
@@ -1,19 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DungeonGuide
 {
 	public class MenuItems
 	{
 		#region public methods
-		[MenuItem("Tools/Align Tiles")]
+		[MenuItem("Tools/Align Tiles %3")]
 		public static void AlignTiles()
 		{
 			SnappableRoot[] allSnappable = GameObject.FindObjectsOfType<SnappableRoot>();
+			Log.Print("Aligning " + allSnappable.Length + " snappable tiles.", LogChannel.DEBUG);
 			foreach(SnappableRoot root in allSnappable)
 			{
 				GridUtility.SnapToGrid(root.gameObject, root.snapType);
+			}
+		}
+		
+		[MenuItem("Tools/Delete Duplicates")]
+		public static void DeleteDuplicates()
+		{
+			List<SnappableRoot> allSnappable = GameObject.FindObjectsOfType<SnappableRoot>().ToList();
+			List<SnappableRoot> dupsToDelete = new List<SnappableRoot>();
+			List<SnappableRoot> uniquesToKeep = new List<SnappableRoot>();
+			
+			foreach(SnappableRoot root in allSnappable)
+			{
+				SnappableRoot foundInUniques = uniquesToKeep.Find(found => 
+				                                                  found.name == root.name && 
+				                                                  found.transform.position == root.transform.position);
+				
+				if (foundInUniques == null)
+				{
+					uniquesToKeep.Add(root);
+				}
+				else
+				{
+					dupsToDelete.Add(root);
+				}
+			}
+			
+			Log.Print("Found " + allSnappable.Count + " tiles. Deleted " + dupsToDelete.Count + " duplicates.", LogChannel.DEBUG);
+			
+			foreach(SnappableRoot root in dupsToDelete)
+			{
+				GameObject.DestroyImmediate(root.gameObject);
 			}
 		}
 		
