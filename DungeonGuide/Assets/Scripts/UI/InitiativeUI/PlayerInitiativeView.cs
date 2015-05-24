4a02407a11f1675d2c9d0aceeee24d8f101a737a
dumaike <mdwyer84@gmail.com>
2015-05-24 11:59:26 -0400

    Added pre-selected name options to the initiative dock.

---
+++ DungeonGuide/Assets/Scripts/UI/InitiativeUI/PlayerInitiativeView.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace DungeonGuide
+{
+	public class PlayerInitiativeView : MonoBehaviour 
+	{
+		[SerializeField]
+		private PlayerInitiativeElement playerInitiativeElementTemplate;
+		
+		[SerializeField]
+		private GameObject addPlayerDialog;
+		
+		[SerializeField]
+		private InputField newPlayerName;
+		
+		[SerializeField]
+		private InputField newPlayerInitiative;
+		
+		[SerializeField]
+		private int maxPlayers = 10;
+	
+		List<PlayerInitiativeElement> playerEntries = 
+			new List<PlayerInitiativeElement>();
+			
+		private PlayerInitiativeElement actingElement;
+		
+		public void AddPlayer()
+		{
+			//Clear out double clicks
+			SceneManager.eventCtr.FireMenuItemClickedEvent();			
+			
+			if (this.playerEntries.Count >= this.maxPlayers)
+			{
+				return;
+			}
+			
+			this.newPlayerInitiative.text = "";
+			this.newPlayerName.text = "";
+			this.addPlayerDialog.SetActive(true);
+			
+			//Set the player name as the next input
+			EventSystem system = EventSystem.current;// EventSystemManager.currentSystem;
+			Selectable next = 
+				system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+			
+			system.SetSelectedGameObject(next.gameObject, new BaseEventData(system));
+		}
+		
+		public void ConfirmAddPlayer()
+		{
+			PlayerInitiativeElement newEle =
+				Instantiate(this.playerInitiativeElementTemplate);
+			
+			newEle.transform.SetParent(this.transform, false);
+			
+			int playerInitiative = 0;
+			int.TryParse(this.newPlayerInitiative.text, out playerInitiative);
+			newEle.InitializeElement(this, this.newPlayerName.text, playerInitiative);
+			
+			this.playerEntries.Add(newEle);
+			
+			if (this.playerEntries.Count == 1)
+			{
+				newEle.SetActing();
+				this.actingElement = newEle;
+			}
+			
+			this.playerEntries.Sort((x, y) => (y.initiative.CompareTo(x.initiative)));
+			
+			for (int iPlayerIndex = 0; iPlayerIndex < this.playerEntries.Count; ++iPlayerIndex)
+			{
+				PlayerInitiativeElement player = this.playerEntries[iPlayerIndex];
+				player.transform.SetSiblingIndex(iPlayerIndex);
+			}
+			
+			this.addPlayerDialog.SetActive(false);
+		}
+		
+		public void RemovePlayer(PlayerInitiativeElement elementToRemove)
+		{
+			//Clear out double clicks
+			SceneManager.eventCtr.FireMenuItemClickedEvent();
+			
+			if (elementToRemove == this.actingElement)
+			{
+				AdvanceInitiative();
+			}
+		
+			this.playerEntries.Remove(elementToRemove);			
+			
+			Destroy (elementToRemove.gameObject);
+			
+			if (this.playerEntries.Count == 0)
+			{
+				this.actingElement = null;
+			}
+		}
+		
+		public void AdvanceInitiative()
+		{
+			//Clear out double clicks
+			SceneManager.eventCtr.FireMenuItemClickedEvent();
+		
+			if (this.actingElement != null)
+			{
+				this.actingElement.SetWaiting();
+				
+				int activeElementIndex = this.playerEntries.IndexOf(this.actingElement);
+				activeElementIndex = (activeElementIndex + 1)%this.playerEntries.Count;
+				
+				this.actingElement = this.playerEntries[activeElementIndex];
+				
+				this.actingElement.SetActing();
+			}
+		}
+	}
+}
---
+++ DungeonGuide/Assets/Scripts/UI/InitiativeUI/PlayerNameInitiativeToggles.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace DungeonGuide
+{
+	public class PlayerNameInitiativeToggles : MonoBehaviour 
+	{
+		[SerializeField]
+		private ToggleGroup groupToDriveName;
+		
+		[SerializeField]
+		private InputField nameField;
+		
+		private List<string> childTogglesNames = new List<string>();
+			
+		// Use this for initialization
+		void Start () 
+		{
+			Debug.Log("Called once?");
+			Toggle[] childToggles = GetComponentsInChildren<Toggle>(true);
+			foreach (Toggle toggle in childToggles)
+			{
+				string toggleName = toggle.GetComponentInChildren<Text>().text;
+				this.childTogglesNames.Add(toggleName);
+				toggle.onValueChanged.AddListener(toggled => 
+				{
+					SceneManager.eventCtr.FireMenuItemClickedEvent();	
+					
+					if (toggled)
+					{
+						this.nameField.text = toggleName;
+					}
+				});
+			}
+			
+			this.nameField.onValueChange.AddListener(OnLabelValueChanged);
+		}
+		
+		private void OnLabelValueChanged(string newValue)
+		{
+			if (this.childTogglesNames.Contains(newValue))
+			{
+				return;
+			}
+			
+			this.groupToDriveName.SetAllTogglesOff();
+		}
+	}
+}
