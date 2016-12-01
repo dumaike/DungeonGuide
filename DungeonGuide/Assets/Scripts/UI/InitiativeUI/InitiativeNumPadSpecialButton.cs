61b241b18df0020fc7c4cfb2f172738ec2768409
PaigeWelton <paige@risingtidegames.com>
2016-11-30 21:23:09 -0500

    Added files for the player initiative number pad.

---
+++ DungeonGuide/Assets/Scripts/UI/InitiativeUI/InitiativeNumPadSpecialButton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace DungeonGuide
+{
+    public class InitiativeNumPadSpecialButton : InitiativeNumPadButton
+    {
+        private enum ButtonType
+        {
+            BACKSPACE,
+            NEGATIVE_TOGGLE
+        }
+
+        [SerializeField]
+        private ButtonType buttonType;
+
+        public override void OnButtonPress()
+        {
+
+            //Clear out double clicks
+            SceneManager.eventCtr.FireMenuItemClickedEvent();
+
+            if (buttonType == ButtonType.BACKSPACE && playerInitiativeText.text.Length !=0)
+            {
+
+                playerInitiativeText.text = playerInitiativeText.text.Substring(0, playerInitiativeText.text.Length - 1);
+
+            }
+
+            else if (buttonType == ButtonType.NEGATIVE_TOGGLE && playerInitiativeText.text.Length !=0)
+            {
+                if (playerInitiativeText.text.Substring(0, 1) == "-")
+                {
+                    playerInitiativeText.text = playerInitiativeText.text.Substring(1);
+                }
+                else
+                {
+                    playerInitiativeText.text = "-" + playerInitiativeText.text;
+                }
+            }
+
+        }
+    }
+}
